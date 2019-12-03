3997f657562d2763693aa2a3482f174d0cb65a48
Maxence CADIOT <mcadiot@opnf.fr>
2019-12-03 16:20:14 +0100

    update web3service

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var Web3 = require('web3');
+  var contract = require('truffle-contract');
+  var web3;
+
+  if(typeof web3 !== 'undefined')
+    web3 = new Web3(web3.currentProvider)
+  else 
+    web3 = new Web3(Web3.givenProvider || "https://ropsten.infura.io/v3/5aef76951e4a4689befb73d13839d113");
+  console.log('web3', web3)
+
+  var SC_artifacts = require('./test/PropertyFactory.json')
+  var SC = contract(SC_artifacts)
+  SC.setProvider(web3.currentProvider)
+  if(typeof SC.currentProvider.sendAsync !== 'function') {
+      SC.currentProvider.sendAsync = function() {
+          return SC.currentProvider.send.apply(SC.currentProvider, arguments)
+      }
+  }
+  console.log('SC', SC)
+
+  async function getActor(actorName) {
+    let deployedContract = await SC.deployed()
+    let getActor = await deployedContract.getActor.call(actorName)
+      return getActor;
+    }
+
+  getActor().then((response) => {console.log(response)})
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>TEST</p>
       </header>
     </div>
   );
--- front/src/Services/Web3Service.js
+++ front/src/Services/Web3Service.js
@@ -2,26 +2,22 @@ var Web3 = require('web3');
 var contract = require('truffle-contract');
 
 export default class Web3Service {
-    constructor() {
-        var web3;
-        const SC, SC_artifacts
-    }
-
     init() {
-        if (typeof web3 !== undefined)
+        if (typeof web3 !== 'undefined')
             web3 = new Web3(web3.currentProvider)
         else
-            web3 = new Web3(new Web3.providers.HttpProvider(''))
+            var web3 = new Web3(Web3.givenProvider || "https://ropsten.infura.io/v3/5aef76951e4a4689befb73d13839d113");
     }
 
     loadSmartContract() {
-        SC_artifacts = require('./build/contracts/SC.json')
-        SC = contract(SC_artifacts)
+        var SC_artifacts = require('../test/PropertyFactory.json')
+        var SC = contract(SC_artifacts)
         SC.setProvider(web3.currentProvider)
         if(typeof SC.currentProvider.sendAsync !== 'function') {
             SC.currentProvider.sendAsync = function() {
                 return SC.currentProvider.send.apply(SC.currentProvider, arguments)
             }
         }
+        return SC
     }
 }
