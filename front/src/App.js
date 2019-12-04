0f54efd22292efa2ee77fa4e93e9e9e9252615af
jeremie <jeremieroland@orange.fr>
2019-12-04 14:07:50 +0100

    oubli commit App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+const uuid = require('uuid');
 function App() {
   var Web3 = require('web3');
   var contract = require('truffle-contract');
@@ -12,23 +12,31 @@ function App() {
     web3 = new Web3(Web3.givenProvider || "https://ropsten.infura.io/v3/5aef76951e4a4689befb73d13839d113");
   console.log('web3', web3)
 
-  var SC_artifacts = require('./test/PropertyFactory.json')
+  var SC_artifacts = require('./PropertyFactory.json')
   var SC = contract(SC_artifacts)
   SC.setProvider(web3.currentProvider)
-  if(typeof SC.currentProvider.sendAsync !== 'function') {
+  /*if(typeof SC.currentProvider.sendAsync !== 'function') {
       SC.currentProvider.sendAsync = function() {
           return SC.currentProvider.send.apply(SC.currentProvider, arguments)
       }
-  }
+  }*/
   console.log('SC', SC)
-
-  async function getActor(actorName) {
+  console.log('account', web3.eth.accounts)
+  async function postProperty() {
     let deployedContract = await SC.deployed()
-    let getActor = await deployedContract.getActor.call(actorName)
-      return getActor;
+    await deployedContract.post(1, 20, "20 rue Montorgueil", "un petit appartement", uuid(), 2, {from:web3.eth.accounts.givenProvider.selectedAddress, gas:30000000})
+      
     }
-
-  getActor().then((response) => {console.log(response)})
+  
+    async function getPropertyById() {
+      console.log('entry')
+      let deployedContract = await SC.deployed()
+      let p=await deployedContract.properties.call(0)
+      console.log('p',p)
+        return p
+      }
+      console.log('>>>>')
+    postProperty().then( (resp) => console.log('resp=',resp))
 
   return (
     <div className="App">
