4c5fd905443f841a35d24c617726acbd7e4561c0
Maxence CADIOT <mcadiot@opnf.fr>
2019-12-06 22:55:51 +0100

    PropertiesCatalog

---
+++ front/src/components/PropertiesCatalogComponent/PropertiesCatalogComponent.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+
+let Web3 = require('web3');
+let contract = require('truffle-contract');
+let web3, deployedContract;
+
+if(typeof web3 !== 'undefined')
+    web3 = new Web3(web3.currentProvider)
+else 
+    web3 = new Web3(Web3.givenProvider || "https://ropsten.infura.io/v3/5aef76951e4a4689befb73d13839d113");
+
+let SC_artifacts = require('../../PropertyFactory.json')
+let SC = contract(SC_artifacts)
+SC.setProvider(web3.currentProvider)
+
+export default class PropertiesCatalogComponent extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            nbProperties: null,
+        }
+    }
+
+    componentDidMount() {
+        this.getNbProperties()
+    }
+
+    async getNbProperties() {
+        let { properties } = this.state
+        deployedContract = await SC.deployed()
+        let nbProperties = await deployedContract.getNbProperties()
+
+        for (let i = 0; i < nbProperties; i++) {
+            let property = await this.getPropertyById(i)
+            console.log('Property n° ' + i + ' : ', property)
+        }
+
+        this.setState({ nbProperties: nbProperties.toNumber() })
+    }
+
+    async getPropertyById(id) {
+        deployedContract = await SC.deployed()
+        let property = await deployedContract.properties.call(id)
+        return property
+    }
+
+    render() {
+        const { nbProperties } = this.state
+        return (
+            <div>
+                <p>Nombre de propriétés : { nbProperties }</p>
+            </div>
+        )
+    }
+}
--- front/src/index.js
+++ front/src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import PropertiesCatalogComponent from './components/PropertiesCatalogComponent/PropertiesCatalogComponent.js'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path='/' component={ PropertiesCatalogComponent } />
+    </Switch>
+  </Router>
+)
+
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
